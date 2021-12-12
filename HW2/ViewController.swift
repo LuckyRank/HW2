b1871cdc905798ba15b4f1c32152f0fbdc54d90b
Danil Prozorov <Whale8shark999@gmail.com>
2021-12-12 23:30:03 +0300

    trying to realise  logic

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var viewColored: UIView!
+    
+    @IBOutlet weak var redCounter: UILabel!
+    @IBOutlet weak var greenCounter: UILabel!
+    @IBOutlet weak var blueCounter: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewColored.layer.cornerRadius = 15
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        var redSliderValue = CGFloat(redSlider.value)
+        var greenSliderValue = CGFloat(greenSlider.value)
+        var blueSliderValue = CGFloat(blueSlider.value)
+        
+        viewColored.backgroundColor = .init(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: <#CGFloat#>)
+    }
+    
+    @IBAction func redSliderAction() {
+        redCounter.text = String(redSlider.value)
+        var redSliderValue = CGFloat(redSlider.value)
+    }
+    
+    
+    @IBAction func greenSliderAction() {
+        greenCounter.text = String(greenSlider.value)
+       var greenSliderValue = CGFloat(greenSlider.value)
     }
 
-
+   @IBAction func blueSliderAction() {
+       blueCounter.text = String(blueSlider.value)
+        var blueSliderValue = CGFloat(blueSlider.value)
+   }
+    
+    
 }
+
 
